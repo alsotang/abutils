9d0069a331b3d107174b6f24493fb536330898e4
alsotang <alsotang@gmail.com>
2022-11-16 20:25:25 +0800

    use typescript

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const abutils = {
+  slice(ab: AudioBuffer, start?: number, end?: number): AudioBuffer { },
+  repeat(ab: AudioBuffer, times: number): AudioBuffer {
+  },
+  concat(abs: AudioBuffer[]): AudioBuffer { },
+  clone(ab: AudioBuffer): AudioBuffer { }
+}
+
+export { abutils };
